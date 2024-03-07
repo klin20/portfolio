c4c9135c9b58c1c1a17c6a48e0bd7d5ad94ab882
klin20 <klin18@u.rochester.edu>
2024-03-06 22:27:29 -0600

    huge portfolio changes
    work, about, and resume updates; added one project; started mobile responsiveness; changed font styles

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 // Get the button:
 let mybutton = document.getElementById("backToTop");
+let mybutton2 = document.getElementById("explore");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the back to top button and hide the explore motion
 window.onscroll = function () {
   scrollFunction();
  };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $('.flexNav').css({'background-color':"white", "box-shadow":"0 0.25rem 0.5rem -0.5rem #333333"});
     mybutton.style.display = "block";
+    mybutton2.style.display = "none";
   } else {
+    $('.flexNav').css({'background-color':"rgba(255, 255, 255, 0.5)","box-shadow":"none"});
     mybutton.style.display = "none";
+    mybutton2.style.display = "flex";
   }
 }
 
@@ -20,7 +25,8 @@ function backToTop() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-$(".menuIcon").on("click", function(){
+// mobile menu
+$("#mobileMenu").on("click", function(){
   if($(".menuIcon").hasClass("rotate")) {
     $(".menuIcon").removeClass("rotate");
     $(".menuIcon").css("transition", "all 1s ease-in-out");
